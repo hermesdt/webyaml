153bc5f54c55e4fd3805c738be9809a66bc14f7c
Álvaro Durán Tovar <hermesdt@gmail.com>
2011-11-18 18:20:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webyaml::Application.config.secret_token = '5a03b26a52376b768b8e78e0a20f00b197cc7e7c2fe34459eaf7674e3999edfc6f1521588913fecc974a2138e9588b380ed2cfb117735eb23c0250589de03379'
